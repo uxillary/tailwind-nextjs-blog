b6eaa15650d2525c4839ce4c6247653d9274d7b2
adamski <adamjch@proton.me>
2024-12-06 01:57:19 +0000

    tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,8 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.pink,
+        primary: '#ff6347',
+        secondary: '#1e90ff',
         gray: colors.gray,
       },
       zIndex: {
