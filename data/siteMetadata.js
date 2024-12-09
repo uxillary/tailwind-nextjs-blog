d2a52d0e8cb97d2fe1b4bbb851537919a8653608
adamski <mail@ajstudios.online>
2024-12-09 00:53:55 +0000

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/uxillary/tailwind-nextjs-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
@@ -45,9 +45,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+     googleAnalytics: {
+       googleAnalyticsId: '', // e.g. G-XXXXXXX
+     },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
@@ -108,7 +108,7 @@ module.exports = {
   language: 'en-us', 
   siteUrl: 'https://infinitecurios.blog', 
   email: 'contact@infinitecurios.blog', 
-  github: 'https://github.com/infinitecurios', 
+  github: 'https://github.com/uxillary/infinitecurios', 
   twitter: 'https://twitter.com/InfiniteCurios', 
   linkedin: '', 
 };
